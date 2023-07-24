7cbdd8587f05de828a58873df26be37ef0dae465
iamvince24 <yohongguo24@gmail.com>
2023-07-24 22:38:42 +0800

    New project

---
+++ src/webtitle.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./webtitle.scss";
+
+const Webtitle = () => {
+  return (
+    <>
+      <div className="web-title">
+        <h1 className="title">CAPSTONS STORE</h1>
+      </div>
+    </>
+  );
+};
+
+export default Webtitle;
---
+++ title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const webtitle = () => {
+  return (
+    <>
+      <div className="web-title">
+        <h1 className="title">CAPSTONS STORE</h1>
+        <hr className="title-underline"></hr>
+      </div>
+    </>
+  );
+};
+
+export default webtitle;
